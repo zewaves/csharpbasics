6e2804bc0323237dcdbd48f8330218ace924c09c
zewaves <m1crowavesze@gmail.com>
2025-08-03 12:23:48 +0530

    Updated First Program

--- Hello.cs
+++ Hello.cs
@@ -1,9 +1,16 @@
-using System;
+using System;	// System namespace
 
 namespace Hello{
 	class displayHello{
 		static void Main(string[] args){
-			Console.WriteLine("HELLO!");
+			// Console class form System
+			// WriteLine, ReadLine methods from Console
+			Console.WriteLine("Enter your name : ");
+			string name = Console.ReadLine();
+			Console.WriteLine("HELLO! " + name);
+			Console.WriteLine("Enter your age : ");
+			int age = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Age " + age);
 		}
 	}
 }
